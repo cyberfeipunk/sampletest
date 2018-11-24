1e8488d5bb2eaa59a74ac3db0b93868b4917a238
vagrant <vagrant@homestead>
2018-11-24 15:19:13 +0000

    tests

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function test(){
+      var_dump(getenv('IS_IN_HEROKU'));
       $url = parse_url(getenv("DATABASE_URL"));
       return $url;
     }
